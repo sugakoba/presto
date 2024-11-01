e43df14fe48a39259953e9b9e5c20eb265cf5213
Henry Li <h1542795993@gmail.com>
2024-11-01 23:01:26 +1100

    Initial set up, including created page and component folder, added axios and react router dom library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,34 @@ import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
+
   
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button id="counter" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </>
   )
 }
 
 export default App
+
+//axios and react-router-dom has been set up, refer to package.json
+
+//To run frontend it is: nvm use
+//npm run dev
+//port: 3000
+
+//To run backend it is: nvm use
+//npm start
+//port: 5005
+
+// TODO: 
+// Need to set up different folders, also remember to npm install. 
+// set up linting, test comes last. 
+
+
+//If there is a library exists go use it. 
+//Hayden Lecture from 1 hour 30mins focuses on dashboard. 
+
