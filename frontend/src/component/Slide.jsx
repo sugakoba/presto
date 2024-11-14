0296017be9059c3a301d8e0998f0bfc9715b23c8
Henry Li <h1542795993@gmail.com>
2024-11-14 18:14:45 +1100

    Fully finished section 2.3.4

--- frontend/src/component/Slide.jsx
+++ frontend/src/component/Slide.jsx
@@ -641,7 +641,83 @@ const Slide = ({ fade, currentSlideIndex, slides, presentation, updatePresentati
                         
                 </AddElementContainer>
             </Modal>
-            </>
+
+            
+            <Modal open={isCodeEditModalOpen} onClose={handleCloseTextEdit}>
+                <AddElementContainer>
+                    <AddElementTitle variant="h5" component="h2">
+                        Edit Code Block
+                    </AddElementTitle>
+                    <AddElementInput
+                        required
+                        value={selectedElement.height}
+                        onChange={(e) => handleElementChange('height', e.target.value)}
+                        label="Edit Code Block Height"
+                        variant="outlined"
+                        margin="normal"
+                    />
+                    <AddElementInput
+                        required
+                        value={selectedElement.width}
+                        onChange={(e) => handleElementChange('width', e.target.value)}
+                        label="Edit Code Block Width"
+                        variant="outlined"
+                        margin="normal"
+                    />
+
+                    <AddElementInput 
+                        required
+                        value={selectedElement.size}
+                        onChange={(e) => handleElementChange('size', e.target.value)}
+                        label="Edit Code Size in em"
+                        variant="outlined"
+                        margin="normal"
+                    />
+
+                    <AddElementInput
+                        required
+                        value={selectedElement.code}
+                        onChange={(e) => handleElementChange('code', e.target.value)}
+                        label="Edit Code Block"
+                        variant="outlined"
+                        margin="normal"
+                        multiline
+                        minRows={4}
+                        fullWidth
+
+                    />
+
+                    <AddElementInput 
+                        required
+                        value={String(selectedElement?.xpos) || ''}
+                        onChange={(e) => handleElementChange('xpos', e.target.value)}
+                        label="Edit X-coordinate"
+                        variant="outlined"
+                        margin="normal"
+                    />
+                    <AddElementInput 
+                        required
+                        value={String(selectedElement?.ypos) || ''}
+                        onChange={(e) => handleElementChange('ypos', e.target.value)}
+                        label="Edit Y-coordinate"
+                        variant="outlined"
+                        margin="normal"
+                    />
+
+                    <div>
+                        <SaveButton variant="contained" onClick={handleElementSave} startIcon={<CheckIcon />}>
+                            Save
+                        </SaveButton>
+                        <CancelButton variant="outlined" onClick={handleCloseCodeModal} startIcon={<CloseIcon />}>
+                            Cancel
+                        </CancelButton>
+                    </div>
+                    
+                </AddElementContainer>
+            </Modal>
+        
+        
+        </>
     );
 };
 
