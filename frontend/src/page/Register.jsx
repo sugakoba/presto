8ba48104c6afcbe0450e68fcbf6b4ee3d0aa130c
Henry Li <h1542795993@gmail.com>
2024-11-15 02:16:51 +1100

    Made the Register file eslint compititable

--- frontend/src/page/Register.jsx
+++ frontend/src/page/Register.jsx
@@ -40,119 +40,119 @@ const BackButton = styled(Button) `
 `;
 
 function Register({ token, handleSuccess }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [name, setName] = useState('');
-    const [isErrorOpen, setErrorOpen] = useState(false);
-    const[errorMsg, setErrorMsg] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [name, setName] = useState('');
+  const [isErrorOpen, setErrorOpen] = useState(false);
+  const[errorMsg, setErrorMsg] = useState('');
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const register = () => {
-        if (!email || !password) {
-            setErrorMsg("Email and password can't be empty.");
-            setErrorOpen(true);
-            return;
-        }
-
-        if (password === confirmPassword) {
-            axios.post('http://localhost:5005/admin/auth/register', {
-                email: email,
-                password: password,
-                name: name,
-            }).then(function (response) {
-                handleSuccess(response.data.token);
-            }).catch(function (error) {
-                setErrorMsg(error.response.data.error);
-                setErrorOpen(true);
-            })
-        } else {
-            setErrorMsg('Password does not match confirm Password')
-            setErrorOpen(true);
-        }
+  const register = () => {
+    if (!email || !password) {
+      setErrorMsg("Email and password can't be empty.");
+      setErrorOpen(true);
+      return;
     }
 
-    const enterKey = (event) => {
-        if (event.key === 'Enter') {
-            register();
-        }
+    if (password === confirmPassword) {
+      axios.post('http://localhost:5005/admin/auth/register', {
+        email: email,
+        password: password,
+        name: name,
+      }).then((response) => {
+        handleSuccess(response.data.token);
+      }).catch((error) => {
+        setErrorMsg(error.response.data.error);
+        setErrorOpen(true);
+      })
+    } else {
+      setErrorMsg('Password does not match confirm Password')
+      setErrorOpen(true);
     }
+  }
+
+  const enterKey = (event) => {
+    if (event.key === 'Enter') {
+      register();
+    }
+  }
 
     
-    const handleCloseError = () => {
-        setErrorOpen(false);
+  const handleCloseError = () => {
+    setErrorOpen(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', enterKey);
+
+    return () => {
+      window.removeEventListener('keydown', enterKey);
     }
+  }, [email, password, confirmPassword, name]);
 
-    useEffect(() => {
-        window.addEventListener('keydown', enterKey);
-
-        return () => {
-            window.removeEventListener('keydown', enterKey);
-        }
-    }, [email, password, confirmPassword, name]);
-
-    return (
-        <>
-            <BackgroundContainer>
-                <RegisterContainer>
-                    <Typography variant="h4" component="h1" align="center">
+  return (
+    <>
+      <BackgroundContainer>
+        <RegisterContainer>
+          <Typography variant="h4" component="h1" align="center">
                             Register
-                    </Typography>
+          </Typography>
 
-                    <TextField 
-                            label="Email"
-                            variant="outlined"
-                            type="email"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                            sx={{ width: '80%' }}
-                    />
+          <TextField 
+            label="Email"
+            variant="outlined"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            sx={{ width: '80%' }}
+          />
 
-                    <TextField 
-                            label="Name"
-                            variant="outlined"
-                            type="text"
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                            sx={{ width: '80%' }}
-                    />
+          <TextField 
+            label="Name"
+            variant="outlined"
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            sx={{ width: '80%' }}
+          />
 
-                    <TextField 
-                        label="Password"
-                        variant="outlined"
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)} 
-                        sx={{ width: '80%' }}
-                    />
+          <TextField 
+            label="Password"
+            variant="outlined"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)} 
+            sx={{ width: '80%' }}
+          />
 
-                    <TextField 
-                        label="Confirm Password"
-                        variant="outlined"
-                        type="password"
-                        value={confirmPassword}
-                        onChange={e => setConfirmPassword(e.target.value)}
-                        sx={{ width: '80%' }}
-                    />
+          <TextField 
+            label="Confirm Password"
+            variant="outlined"
+            type="password"
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+            sx={{ width: '80%' }}
+          />
 
-                    <Button 
-                        variant="contained"
-                        color="primary"
-                        onClick={register}
-                        sx={{ width: '80%' }}
-                    >
+          <Button 
+            variant="contained"
+            color="primary"
+            onClick={register}
+            sx={{ width: '80%' }}
+          >
                         Register
-                    </Button>
+          </Button>
 
-                    <BackButton onClick={() => navigate('/welcome')}>Back</BackButton>
-                </RegisterContainer>
-            </BackgroundContainer>
+          <BackButton onClick={() => navigate('/welcome')}>Back</BackButton>
+        </RegisterContainer>
+      </BackgroundContainer>
 
-            <ErrorPopUp isOpen={isErrorOpen} onClose={handleCloseError} message = {errorMsg}>
-            </ErrorPopUp>
-        </>
-    )
+      <ErrorPopUp isOpen={isErrorOpen} onClose={handleCloseError} message = {errorMsg}>
+      </ErrorPopUp>
+    </>
+  )
 }
 
 export default Register;
